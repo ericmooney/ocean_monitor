0acc2cc74caac51ea55c1e444bca1709cc63cc1e
Eric Mooney <eric.c.mooney@gmail.com>
2014-04-26 19:28:18 -0700

    add personal info fields to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  # Since we've added additional fields to the user mode, we need to make sure they are permitted as well in forms
+  before_filter :configure_devise_params, if: :devise_controller?
+    def configure_devise_params
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:first_name, :last_name, :city, :state, :country, :email, :password, :password_confirmation)
+      end
+    end
+
 end
